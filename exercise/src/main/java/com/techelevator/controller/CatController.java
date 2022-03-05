5e68528bf3614c3a8e43004fc5ab90a10e5969af
Jay Chen <jaycsun1@gmail.com>
2022-03-05 09:31:11 -0500

    Created 2/3 of the get methods in controller

--- exercise/src/main/java/com/techelevator/controller/CatController.java
+++ exercise/src/main/java/com/techelevator/controller/CatController.java
@@ -1,8 +1,19 @@
 package com.techelevator.controller;
 
 import com.techelevator.dao.CatCardDao;
+import com.techelevator.model.CatCard;
 import com.techelevator.services.CatFactService;
 import com.techelevator.services.CatPicService;
+import com.techelevator.services.RestCatFactService;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 
 public class CatController {
 
@@ -16,5 +27,19 @@ public class CatController {
         this.catPicService = catPicService;
     }
 
+    @RequestMapping(path = "/api/cards", method = RequestMethod.GET)
+    public List<CatCard> getCatCards() {
+        return this.catCardDao.list();
+    }
+
+    @RequestMapping(path = "/api/cards/{id}", method = RequestMethod.GET)
+    public CatCard getCatCard(@PathVariable int id) {
+        return this.catCardDao.get(id);
+    }
+//
+//    @RequestMapping(path = "/api/cards/random", method = RequestMethod.GET)
+//    public CatCard getRandomCatCard() {
+//        CatCard randomCatCard = RestCatFactService.getFact(
+//    }
 
 }
--- exercise/src/main/java/com/techelevator/services/RestCatPicService.java
+++ exercise/src/main/java/com/techelevator/services/RestCatPicService.java
@@ -3,14 +3,30 @@ package com.techelevator.services;
 import org.springframework.stereotype.Component;
 
 import com.techelevator.model.CatPic;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestClientResponseException;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
 
 @Component
 public class RestCatPicService implements CatPicService {
 
+	private static final String API_BASE_URL = "https://cat-data.netlify.app/api/pictures/random";
+	private RestTemplate restTemplate = new RestTemplate();
+
 	@Override
 	public CatPic getPic() {
-		// TODO Auto-generated method stub
-		return null;
+		CatPic catPic = null;
+
+		try {
+			catPic = restTemplate.getForObject(API_BASE_URL, CatPic.class);
+		} catch (RestClientResponseException e) {
+			System.out.println(e.getRawStatusCode());
+		} catch (ResourceAccessException e) {
+			System.out.println(e.getMessage());
+		}
+		return catPic;
 	}
 
 }	
